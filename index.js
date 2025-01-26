0661098773b8eb7b74f0b470e58446a78e728568
Amanverma1802-eng <av51217043@gmail.com>
2025-01-26 20:58:22 +0530

    rendering data to show.ejs

--- index.js
+++ index.js
@@ -7,12 +7,16 @@ app.set("view engine","ejs");
 app.use(express.json());
 app.use(express.urlencoded({extended:true}));
 app.use(express.static(path.join(__dirname,'public')));
-app.get('/',function(req,res)
-{
-    fs.readdir(`./files`,function(err,files)
-{
+app.get('/',function(req,res){
+    fs.readdir(`./files`,function(err,files){
     res.render("index",{files:files});
 });
+})
+
+app.get('/files/:filename',function(req,res){
+    fs.readFile(`./files/${req.params.filename}`,"utf-8",function(err,filedata){
+        res.render(`show`,{filename:req.params.filename , filedata:filedata});
+    })
 })
 
 app.post('/create',function(req,res){
